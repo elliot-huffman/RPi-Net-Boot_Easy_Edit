83cd9f4b265f6f3be2a754ac6f0785d41aa14de9
Elliot Huffman <thelich2@gmail.com>
2016-02-09 12:53:54 -0500

    Initial Tool Creation
    Initial file and programming for the tool.

---
+++ Mode-Toggle.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+servermode () {
+  chown root:root -R /var/lib/tftpboot/
+  chown root:root -R /var/www/html/
+  chmod 755 -R /var/lib/tftpboot/
+  chmod 755 -R /var/www/html/
+  mainmenu
+  }
+
+editmode () {
+  chown elliot:elliot -R /var/www/html/
+  chown elliot:elliot -R /var/lib/tftpboot/
+  mainmenu
+  }
+
+quitprogram () {
+  clear
+  echo "Sorry to see you go... :("
+  exit
+  }
+
+mainmenu () {
+  echo "Press 1 to enable edit mode"
+  echo "Press 2 to enable server mode"
+  echo "Press x to exit the script"
+  read -n 1 -p "Input Selection:" "mainmenuinput"
+  if [ "$mainmenuinput" = "1" ]; then
+            clear
+            editmode
+        elif [ "$mainmenuinput" = "2" ]; then
+            clear
+            servermode
+        elif [ "$mainmenuinput" = "x" ];then
+            clear
+            quitprogram
+        elif [ "$mainmenuinput" = "X" ];then
+            clear
+            quitprogram
+        else
+            echo "You have entered an invallid selection!"
+            echo "Please try again!"
+            echo ""
+            echo "Press any key to continue..."
+            read -n 1
+            clear
+            mainmenu
+        fi
+}
+mainmenu
